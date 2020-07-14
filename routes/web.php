23e357fa73cf9135e08ae9048094a73972f83256
boa0421 <boa41109@gmail.com>
2020-07-14 10:36:44 +0000

    add news edit

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::group(['prefix' => 'admin'], function() {
      Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
      Route::post('news/create', 'Admin\NewsController@create');
      Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
 });
 
 Route::group(['prefix' => 'admin'], function() {
