90f917b00ccdaefafa12faac120ec61a6cb8ec2c
boa0421 <boa41109@gmail.com>
2020-07-16 10:41:08 +0000

    add profile edit

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,14 @@ Route::group(['prefix' => 'admin'], function() {
      Route::get('news', 'Admin\NewsController@index')->middleware('auth');
      Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
      Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 Route::group(['prefix' => 'admin'], function() {
      Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
      Route::post('profile/create', 'Admin\ProfileController@create');
      Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-     Route::post('profile/edit', 'Admin\ProfileController@update');
+     Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth');
 });
 Auth::routes();
 
